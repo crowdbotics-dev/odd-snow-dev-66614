773c6e10384e17bf61df1140dbf2989808938405
crowdbotics-dev <engineering@crowdbotics.com>
2023-03-04 01:52:34 +0000

    Update models

--- backend/home/admin.py
+++ backend/home/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Testfyc
+admin.site.register(Testfyc)
 
 # Register your models here.
--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,5 @@
+from django.conf import settings
 from django.db import models
+class Testfyc(models.Model):
+    'Generated Model'
+    test_field_name = models.BigIntegerField()
